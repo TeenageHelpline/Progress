dec34850078345aea72d69ad6929b955d30e57e5
Ben Magee <ben@bmagee.com>
2015-09-04 15:44:53 +0100

    Added JS which makes the progress logo spin

--- resources/views/layouts/includes.blade.php
+++ resources/views/layouts/includes.blade.php
@@ -7,4 +7,18 @@
 
 <!-- JS -->
 <script src="{{ asset('/assets/js/app.js') }}"></script>
+
+<script>
+    $(document).ready(function() {
+
+        $("#progress-brand").hover(function() {
+            // On hover
+            $("#progress-brand i").addClass('fa-spin')
+        }, function() {
+            // End hover
+            $("#progress-brand i").removeClass('fa-spin')
+        })
+
+    })
+</script>
 @endsection
