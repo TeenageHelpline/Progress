ade26f9ff93ad04f8b40186b3833ff9cc95b6dbe
Ben Magee <ben@bmagee.com>
2015-09-04 12:07:21 +0100

    Made some basic changes to the header to link to GitHub as well as a demo menu.

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -15,17 +15,16 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="#">Link</a></li>
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">My Account <span class="caret"></span></a>
                     <ul class="dropdown-menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li>
+                        <li><a href="#">My Settings</a></li>
+                        <li><a href="#">Logout</a></li>
                         <li role="separator" class="divider"></li>
-                        <li><a href="#">Separated link</a></li>
+                        <li><a href="#">Get Help</a></li>
                     </ul>
                 </li>
+                <li><a target="_blank" href="https://www.github.com/TeenageHelpline/Progress"><i class="fa fa-fw fa-github"></i></a></li>
             </ul>
         </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
