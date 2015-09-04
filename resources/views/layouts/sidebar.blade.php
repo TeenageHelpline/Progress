9b60a3df1cee78db980e3f3951aefdd51cdc60c5
Ben Magee <ben@bmagee.com>
2015-09-04 15:29:43 +0100

    Added FontAwesome icons

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,23 +1,23 @@
 @section('sidebar')
     <div class="col-sm-3 col-md-2 sidebar">
         <ul class="nav nav-sidebar">
-            <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
-            <li><a href="#">Reports</a></li>
-            <li><a href="#">Export</a></li>
-            <li><a href="#">Import</a></li>
+            <li class="active"><a href="#"><i class="fa fa-fw fa-dashboard"></i> Overview <span class="sr-only">(current)</span></a></li>
+            <li><a href="#"><i class="fa fa-fw fa-pie-chart"></i> Reports</a></li>
+            <li><a href="#"><i class="fa fa-fw fa-arrow-right"></i> Export</a></li>
+            <li><a href="#"><i class="fa fa-fw  fa-arrow-left"></i> Import</a></li>
         </ul>
         <ul class="nav nav-sidebar">
-            <li><a href="">People</a></li>
-            <li><a href="">Add New Person</a></li>
-            <li><a href="">Time Tracker</a></li>
+            <li><a href=""><i class="fa fa-fw fa-users"></i> People</a></li>
+            <li><a href=""><i class="fa fa-fw fa-user"></i> Add New Person</a></li>
+            <li><a href=""><i class="fa fa-fw fa-clock-o"></i> Time Tracker</a></li>
         </ul>
         <ul class="nav nav-sidebar">
-            <li><a href="">Job Positions</a></li>
-            <li><a href="">New Job Position</a></li>
+            <li><a href=""><i class="fa fa-fw fa-briefcase"></i> Job Positions</a></li>
+            <li><a href=""><i class="fa fa-fw fa-plus"></i> New Job Position</a></li>
         </ul>
         <ul class="nav nav-sidebar">
-            <li><a href="">Job Openings</a></li>
-            <li><a href="">Submitted Applications</a></li>
+            <li><a href=""><i class="fa fa-fw fa-folder-open"></i> Job Openings</a></li>
+            <li><a href=""><i class="fa fa-fw fa-file-text"></i> Submitted Applications</a></li>
         </ul>
     </div>
 @endsection
