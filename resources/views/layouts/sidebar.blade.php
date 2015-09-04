c286cf5729a1a908d16dabbdf1b99a61a5625c92
Ben Magee <ben@bmagee.com>
2015-09-04 15:17:18 +0100

    Made sidebar items more relevant

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,20 +3,20 @@
         <ul class="nav nav-sidebar">
             <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
             <li><a href="#">Reports</a></li>
-            <li><a href="#">Analytics</a></li>
             <li><a href="#">Export</a></li>
+            <li><a href="#">Import</a></li>
         </ul>
         <ul class="nav nav-sidebar">
-            <li><a href="">Nav item</a></li>
-            <li><a href="">Nav item again</a></li>
-            <li><a href="">One more nav</a></li>
-            <li><a href="">Another nav item</a></li>
-            <li><a href="">More navigation</a></li>
+            <li><a href="">People</a></li>
+            <li><a href="">Add New Person</a></li>
         </ul>
         <ul class="nav nav-sidebar">
-            <li><a href="">Nav item again</a></li>
-            <li><a href="">One more nav</a></li>
-            <li><a href="">Another nav item</a></li>
+            <li><a href="">Job Positions</a></li>
+            <li><a href="">New Job Position</a></li>
+        </ul>
+        <ul class="nav nav-sidebar">
+            <li><a href="">Job Openings</a></li>
+            <li><a href="">Submitted Applications</a></li>
         </ul>
     </div>
 @endsection
