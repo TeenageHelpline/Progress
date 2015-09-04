4f028670ba81912a8a3b024bc87faf3cf95f0795
Ben Magee <ben@bmagee.com>
2015-09-04 15:39:21 +0100

    Actually added no-sidebar layout

---
+++ resources/views/layouts/nosidebar.blade.php
@@ -0,0 +1,20 @@
+@include('layouts.header')
+@include('layouts.sidebar')
+@include('layouts.includes')
+<!doctype html>
+<html>
+<head>
+@yield('includes')
+</head>
+<body>
+@yield('header')
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-8 main center-block" style="float: none;">
+            @yield('content')
+        </div>
+    </div>
+</div>
+</body>
+</html>
